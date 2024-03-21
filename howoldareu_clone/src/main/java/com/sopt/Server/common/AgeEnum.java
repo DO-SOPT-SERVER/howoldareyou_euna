be318b21f3b93ce14e0fab0f2633f2c70096e46c
sung-silver <jinna0319@gmail.com>
2024-03-21 22:32:18 +0900

    [FEAT] 서비스 단에서 도메인 로직 분리

--- howoldareu_clone/src/main/java/com/sopt/Server/common/AgeEnum.java
+++ howoldareu_clone/src/main/java/com/sopt/Server/common/AgeEnum.java
@@ -18,5 +18,18 @@ public enum AgeEnum {
     private final String imageUrl1;
     private final String imageUrl2;
 
+    public static AgeEnum getAgeEnum(int age){
+        if(age < 20)
+            return AgeEnum.TEENAGER;
+        else if(age < 30)
+            return AgeEnum.TWENTIES;
+        else if(age < 40)
+            return AgeEnum.THIRTIES;
+        else if(age < 50)
+            return AgeEnum.FORTIES;
+        else
+            return AgeEnum.FIFTIES;
+    }
+
 }
 
--- howoldareu_clone/src/main/java/com/sopt/Server/service/ResultService.java
+++ howoldareu_clone/src/main/java/com/sopt/Server/service/ResultService.java
@@ -19,7 +19,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -41,7 +40,7 @@ public class ResultService {
             Answer answer = answerJpaRepository.findByQuestionAndAnswerType(question, result.answerType()).orElseThrow(()->new CustomException(Error.NOT_FOUND_ANSWER_EXCEPTION,Error.NOT_FOUND_ANSWER_EXCEPTION.getMessage()));
             memberAge += answer.getAnswerScore();
         }
-        AgeEnum ageEnum = getAgeEnum(memberAge);
+        AgeEnum ageEnum = AgeEnum.getAgeEnum(memberAge);
         resultJpaRepository.save(Result.builder().member(member).resultAge(memberAge).build());
         return ResultResponseDTO.of(request.nickname(),memberAge,ageEnum.getTitle(),ageEnum.getContent(),ageEnum.getImageUrl1(), ageEnum.getImageUrl2());
     }
@@ -50,34 +49,16 @@ public class ResultService {
 
         List<Result> resultList  = resultJpaRepository.findAllByMemberIdOrderByIdDesc(memberId);
         List<AllResultsResponseDTO> answer = new ArrayList<>();
-        //멤버의 result 모두 갖고옴 이것을 각각의 result마다 allresultsresponsedto만들어야 해
+
         for(Result result : resultList) {
-            AgeEnum ageEnum = getAgeEnum(result.getResultAge());
-            String time = getStringDate(result.getTestedDate());
+            AgeEnum ageEnum = AgeEnum.getAgeEnum(result.getResultAge());
+            String time = result.getTestedDateToString();
             AllResultsResponseDTO dto = AllResultsResponseDTO.of(result, ageEnum.getTitle(), ageEnum.getContent(), time, ageEnum.getImageUrl1(), ageEnum.getImageUrl2());
             answer.add(dto);
         }
 
         return answer;
 
-    }
-
-    private String getStringDate(LocalDateTime time) {
-        String answer = time.getMonthValue() + "월 " + time.getDayOfMonth() + "일";
-        return answer;
-    }
-
-    private AgeEnum getAgeEnum(int age){
-        if(age < 20)
-            return AgeEnum.TEENAGER;
-        else if(age < 30)
-            return AgeEnum.TWENTIES;
-        else if(age < 40)
-            return AgeEnum.THIRTIES;
-        else if(age < 50)
-            return AgeEnum.FORTIES;
-        else
-            return AgeEnum.FIFTIES;
     }
 
 }
