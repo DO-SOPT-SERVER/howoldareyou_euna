093d3e836422aabb850e32086871f5a79a81df8d
sung-silver <jinna0319@gmail.com>
2024-03-26 12:56:00 +0900

    [FEAT] RestDoc 설정 & 확인

---
+++ howoldareu_clone/src/main/java/com/sopt/Server/common/JpaAuditingConfiguration.java
@@ -0,0 +1,9 @@
+package com.sopt.Server.common;
+
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableJpaAuditing
+public class JpaAuditingConfiguration {
+}
--- howoldareu_clone/src/main/java/com/sopt/Server/controller/MemberController.java
+++ howoldareu_clone/src/main/java/com/sopt/Server/controller/MemberController.java
@@ -5,9 +5,12 @@ import com.sopt.Server.controller.request.MemberPostRequest;
 import com.sopt.Server.controller.response.MemberGetResponse;
 import com.sopt.Server.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 import static com.sopt.Server.exception.Success.CREATE_MEMBER_SUCCESS;
 
@@ -18,7 +21,9 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/member")
-    public ApiResponse<MemberGetResponse> saveMember(@RequestBody MemberPostRequest request) {
-        return ApiResponse.success(CREATE_MEMBER_SUCCESS, memberService.saveMember(request));
+    public ResponseEntity<ApiResponse<MemberGetResponse>> saveMember(@RequestBody MemberPostRequest request) {
+        MemberGetResponse response = memberService.saveMember(request);
+        URI uri = URI.create("/member"+response.memberId());
+        return ResponseEntity.created(uri).body(ApiResponse.success(CREATE_MEMBER_SUCCESS, response));
     }
 }
