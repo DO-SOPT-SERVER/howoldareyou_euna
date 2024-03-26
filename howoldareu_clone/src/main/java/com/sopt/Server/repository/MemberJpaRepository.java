92b5eeaa959c2b8b214f750452d83dacfe6ae201
sung-silver <jinna0319@gmail.com>
2024-03-26 20:04:48 +0900

    [FEAT] MemberJpaRepositoryTest 테스트코드 작성

--- howoldareu_clone/src/main/java/com/sopt/Server/repository/MemberJpaRepository.java
+++ howoldareu_clone/src/main/java/com/sopt/Server/repository/MemberJpaRepository.java
@@ -1,16 +1,19 @@
 package com.sopt.Server.repository;
 
 import com.sopt.Server.domain.Member;
+import com.sopt.Server.exception.model.CustomException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+
+import static com.sopt.Server.exception.Error.NOT_FOUND_MEMBER_EXCEPTION;
 
 public interface MemberJpaRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByName(String name);
     default Member findByNameOrThrow(String name) {
         return findByName(name)
                 .orElseThrow(() ->
-                        new EntityNotFoundException("해당 이름의 회원이 존재하지 않습니다."));
+                        new CustomException(NOT_FOUND_MEMBER_EXCEPTION, NOT_FOUND_MEMBER_EXCEPTION.getMessage()));
     }
 }
