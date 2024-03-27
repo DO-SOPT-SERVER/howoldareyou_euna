4a1e0d32cfa6d62f078f2fa9119114c6706913a6
sung-silver <jinna0319@gmail.com>
2024-03-27 13:55:20 +0900

    [FEAT] ResultJpaRepositoryTest 구현

--- howoldareu_clone/src/test/java/com/sopt/Server/repository/ResultJpaRepositoryTest.java
+++ howoldareu_clone/src/test/java/com/sopt/Server/repository/ResultJpaRepositoryTest.java
@@ -1,12 +1,54 @@
 package com.sopt.Server.repository;
 
+import com.sopt.Server.domain.Member;
+import com.sopt.Server.domain.Result;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
+@ActiveProfiles("test")
 class ResultJpaRepositoryTest {
+    @Autowired
+    ResultJpaRepository resultJpaRepository;
+
+    @Autowired
+    MemberJpaRepository memberJpaRepository;
+
+    @AfterEach
+    void tearDown() {
+        resultJpaRepository.deleteAllInBatch();
+        memberJpaRepository.deleteAllInBatch();
+    }
 
     @Test
     void findAllByMemberIdOrderByIdDesc() {
+        // given
+        Member member = Member.builder().name("euna").realAge(24).build();
+        memberJpaRepository.save(member);
+        resultJpaRepository.save(saveResult(member, 24));
+        resultJpaRepository.save(saveResult(member, 26));
+
+        // when
+        List<Result> results = resultJpaRepository.findAllByMemberIdOrderByIdDesc(member.getId());
+
+        // then
+        assertNotNull(results);
+        assertEquals(2, results.size());
+        assertEquals(26, results.get(0).getResultAge());
+        assertEquals(24, results.get(1).getResultAge());
+
+
+
+    }
+
+    private Result saveResult(Member member, int resultAge) {
+        return Result.builder().member(member).resultAge(resultAge).build();
     }
 }
